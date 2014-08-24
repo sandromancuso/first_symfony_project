682c2dc4cc7c231c3a03ef3da59f5eccfc7f5323
Sandro Mancuso <sandromancuso@yahoo.com>
2014-08-25 00:25:25 +0100

    Route method and requirements configuration

--- src/Client/APIBundle/Controller/RegistrationController.php
+++ src/Client/APIBundle/Controller/RegistrationController.php
@@ -4,12 +4,14 @@ namespace Client\APIBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 
 class RegistrationController extends Controller {
 
     /**
-     * @Route("/registration/location/{barcode}", name="_locations")
+     * @Route("/registration/location/{barcode}", name="_locations", requirements={"barcode" = "\d+"})
+     * @Method("GET")
      */
     public function locationsFor($barcode) {
         $locations = "{
